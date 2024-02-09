4684c8efc046e0b3ffd2f0b25c49c164083b182e
Andrew Haarsager <athaarsager@gmail.com>
2024-02-09 16:40:22 -0600

    Created state in store for storing movieDetails

--- src/redux/store.js
+++ src/redux/store.js
@@ -57,11 +57,21 @@ const genres = (state = [], action) => {
   }
 }
 
+const movieDetails = (state = [], action) => {
+  switch(action.type) {
+    case "DISPLAY_MOVIE_DETAILS":
+      return action.payload;
+    default:
+  return state;
+  }
+}
+
 // Create one store that all components can use
 const storeInstance = createStore(
   combineReducers({
     movies,
     genres,
+    movieDetails
   }),
   // Add sagaMiddleware to our store
   applyMiddleware(sagaMiddleware, logger),
