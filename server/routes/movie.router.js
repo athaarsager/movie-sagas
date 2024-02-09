29b62964d437e2448b1737fa73629e48965e089b
Andrew Haarsager <athaarsager@gmail.com>
2024-02-09 16:26:12 -0600

    Created sql text for get details route

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+// GET all movies:
+
 router.get('/', (req, res) => {
   const query = `
     SELECT * FROM "movies"
@@ -17,6 +19,23 @@ router.get('/', (req, res) => {
     })
 
 });
+
+// GET details of single movie:
+router.get("/:id", (req, res) => {
+  const movieId = req.params.id;
+
+  // Use STRING_AGG to combine multiple rows into one. Otherwise, will get multiple rows of the same movie if it has multiple genres
+  // JSON_AGG is another great alternative if I want the combined rows to be an array I can iterate over
+  const queryText = `
+  SELECT "movies"."title", "movies"."poster", "movies"."description", STRING_AGG("genres"."name", ', ') AS "genre" FROM "movies"
+  JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+  JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+  WHERE "movies"."id" = $1
+  GROUP BY "movies"."title", "movies"."poster", "movies"."description";
+  `;
+  
+
+})
 
 router.post('/', (req, res) => {
   console.log(req.body);
