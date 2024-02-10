b8f58c868a8a0f7faf3c0e99ae66de66e1a38619
Andrew Haarsager <athaarsager@gmail.com>
2024-02-09 19:00:06 -0600

    Added comments and made minor html adjustments in MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -8,6 +8,7 @@ function MovieDetails() {
     // Grabs id from url. The variable name needs to match what it is called in the route defined in App.jsx
     const { movie_id } = useParams();
 
+    // Tell the database what movie info to grab and put in store on page load. Redux updates and React renders
     useEffect(() => {
         dispatch({type: "GET_DETAILS", payload: movie_id});
     }, []);
@@ -16,8 +17,9 @@ function MovieDetails() {
         <div data-testid="movieDetails">
             <h2>{movieDetails.title}</h2>
             <img src={movieDetails.poster} />
+            <p><strong>Details:</strong></p>
             <p>{movieDetails.description}</p>
-            <p>Genres: {movieDetails.genres}</p>
+            <p><strong>Genres:</strong> {movieDetails.genres}</p>
         </div>
     )
 }
