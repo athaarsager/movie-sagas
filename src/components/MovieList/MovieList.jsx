a546d09b209bf0e05b8f077bfc675791352ffc93
Andrew Haarsager <athaarsager@gmail.com>
2024-02-10 17:19:03 -0600

    Fixed card display!

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,6 +6,8 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import './MovieList.css';
 
 function MovieList() {
@@ -20,18 +22,24 @@ function MovieList() {
   return (
     <main>
       <h1>MovieList</h1>
-      <Button variant="contained" onClick={() => history.push("/add_movie")}>Add a New Movie!</Button>
+      <Button variant="contained" sx={{ marginBottom: 2 }} onClick={() => history.push("/add_movie")}>Add a New Movie!</Button>
       <section className="movies">
-        {movies.map(movie => {
-          return (
-            <Card variant="outlined" sx={{width: 300}} data-testid='movieItem' key={movie.id} onClick={() => history.push(`/movies/${movie.id}`)}>
-                <CardMedia component="img" image={movie.poster} title={movie.title} sx={{ height: 450}}/>
-              <CardContent>
-                <Typography variant="h4" component="div">{movie.title}</Typography>
-              </CardContent>
-            </Card>
-          );
-        })}
+        <Box xs={{ width: "100%" }}>
+          <Grid container rowSpacing={1} columnSpacing={1}>
+            {movies.map(movie => {
+              return (
+                <Grid item xs={2}>
+                  <Card variant="outlined" data-testid='movieItem' key={movie.id} onClick={() => history.push(`/movies/${movie.id}`)}>
+                    <CardMedia component="img" image={movie.poster} alt={movie.title}  />
+                    <CardContent>
+                      <Typography variant="h4" component="div">{movie.title}</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
       </section>
     </main>
   );
