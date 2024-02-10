c29b94c691dbbef59212a1a665f3d0cc2dd02b41
Andrew Haarsager <athaarsager@gmail.com>
2024-02-10 09:02:42 -0600

    Added required tags to input elements

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -12,17 +12,20 @@ function AddMovie() {
         //Path="FETCH_GENRES"
          dispatch({type: "FETCH_GENRES"});
     }, []);
-
+    // TO-DO:
+    // Make form fields required
+    // Add buttons specified in instructions
+    // Make dispatch work
     return (
         <form>
             <label htmlFor="title">Movie Title</label><br/>
-            <input id="title" name="title" type="text" placeholder="Movie Title"/><br/>
+            <input id="title" name="title" type="text" placeholder="Movie Title" required/><br/>
             <label htmlFor="url">Poster Url</label><br/>
-            <input id="url" name="url" type="url" placeholder="https://m.media-amazon.com/images/I/51XhnMdSQdL._AC_UF894,1000_QL80_.jpg"/><br/>
+            <input id="url" name="url" type="url" placeholder="https://m.media-amazon.com/images/I/51XhnMdSQdL._AC_UF894,1000_QL80_.jpg" required/><br/>
             <label htmlFor="description">Description</label><br/>
-            <input id="description" name="description" type="text" placeholder="Kung Fu Panda is a children's animated movie starring Jack Black as the titular character: Po. It follows Po on his quest to become the Dragon Warrior..."/><br/>
+            <input id="description" name="description" type="text" placeholder="Kung Fu Panda is a children's animated movie starring Jack Black as the titular character: Po. It follows Po on his quest to become the Dragon Warrior..." required/><br/>
             <label htmlFor="genre">Genre</label><br/>
-            <select id="genre" name="genre">
+            <select id="genre" name="genre" required>
                 {/* Selected sets the below option as the default. Disabled prevents users from clicking it. Hidden prevents it from showing up in the dropdown menu */}
                 <option value="" selected disabled hidden>--Select Genre--</option>
                 {genres.map(genre => (
