d4e943b7410643975d51d5514edca0d19ed3e34a
Andrew Haarsager <athaarsager@gmail.com>
2024-02-10 13:44:39 -0600

    Created and imported EditDetails Page. Can navigate to it from movieDetails page

---
+++ src/components/MovieDetails/EditDetails.jsx
@@ -0,0 +1,12 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector, useDispatch } from "react-redux";
+
+function EditDetails() {
+    return (
+        <>
+        <h2>EDIT</h2>
+        </>
+    );
+}
+
+export default EditDetails;
