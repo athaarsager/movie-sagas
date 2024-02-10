f5828f33aefff29804f8ae2bd84292dce821bef0
Andrew Haarsager <athaarsager@gmail.com>
2024-02-09 20:24:38 -0600

    Created the base inputs for the AddMovie form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,13 +1,23 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function AddMovie() {
     const dispatch = useDispatch();
     const history = useHistory();
-    
+
     return (
-        <>
-        </>
+        <form>
+            <label for="title">Movie Title</label><br/>
+            <input id="title" name="title" type="text" placeholder="Movie Title"/>
+            <label for="url">Poster Url</label><br/>
+            <input id="url" name="url" type="url" placeholder="https://m.media-amazon.com/images/I/51XhnMdSQdL._AC_UF894,1000_QL80_.jpg"/>
+            <label for="description">Description</label><br/>
+            <input id="description" name="description" type="text" placeholder="Kung Fu Panda is a children's animated movie starring Jack Black as the titular character: Po. It follows Po on his quest to become the Dragon Warrior..."/>
+            <label for="genre">Genre</label><br/>
+            <select id="genre" name="genre">
+                
+            </select>
+        </form>
     )
 }
 
