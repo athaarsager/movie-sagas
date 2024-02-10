c615d010b02c1b3c736a3759e6bb5c95466efd3f
Andrew Haarsager <athaarsager@gmail.com>
2024-02-10 16:18:14 -0600

    Installed MUI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,10 @@ import MovieList from '../MovieList/MovieList';
 import MovieDetails from '../MovieDetails/MovieDetails';
 import EditDetails from '../MovieDetails/EditDetails';
 import AddMovie from '../AddMovie/AddMovie';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
 function App() {
