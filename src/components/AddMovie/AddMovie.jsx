e0796783a7fb039dbe4e9542b4d78e5b7c180a66
Andrew Haarsager <athaarsager@gmail.com>
2024-02-10 09:24:43 -0600

    Made it so all inputs set local state in AddMovie.jsx

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,11 +1,12 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function AddMovie() {
     const dispatch = useDispatch();
     const history = useHistory();
     const genres = useSelector(store => store.genres);
+    const [newMovie, setNewMovie] = useState({});
 
     //need to fetch genres right away so drop down has something to display
     useEffect(() => {
@@ -19,22 +20,23 @@ function AddMovie() {
         e.preventDefault();
         // Path="ADD_MOVIE"
         // Send all the movie info as an object
+        dispatch({type: "ADD_MOVIE", })
     }
 
     return (
         <form>
             <label htmlFor="title">Movie Title</label><br/>
-            <input id="title" name="title" type="text" placeholder="Movie Title" required/><br/>
+            <input id="title" name="title" type="text" placeholder="Movie Title" value={newMovie.title} onChange={(e) => setNewMovie({title: e.target.value})} required/><br/>
             <label htmlFor="url">Poster Url</label><br/>
-            <input id="url" name="url" type="url" placeholder="https://m.media-amazon.com/images/I/51XhnMdSQdL._AC_UF894,1000_QL80_.jpg" required/><br/>
+            <input id="url" name="url" type="url" placeholder="https://m.media-amazon.com/images/I/51XhnMdSQdL._AC_UF894,1000_QL80_.jpg" value={newMovie.poster} onChange={(e) => setNewMovie({poster: e.target.value})} required/><br/>
             <label htmlFor="description">Description</label><br/>
-            <input id="description" name="description" type="text" placeholder="Kung Fu Panda is a children's animated movie starring Jack Black as the titular character: Po. It follows Po on his quest to become the Dragon Warrior..." required/><br/>
+            <input id="description" name="description" type="text" placeholder="Kung Fu Panda is a children's animated movie starring Jack Black as the titular character: Po. It follows Po on his quest to become the Dragon Warrior..." value={newMovie.description} onChange={(e) => setNewMovie({description: e.target.value})} required/><br/>
             <label htmlFor="genre">Genre</label><br/>
             <select id="genre" name="genre" required>
                 {/* Selected sets the below option as the default. Disabled prevents users from clicking it. Hidden prevents it from showing up in the dropdown menu */}
                 <option value="" selected disabled hidden>--Select Genre--</option>
                 {genres.map(genre => (
-                    <option value={genre.name}>{genre.name}</option>
+                    <option onClick={(e) => setNewMovie({genre_id: genre.id})} value={genre.name}>{genre.name}</option>
                 ))}
             </select><br/>
             <button>Save</button>
